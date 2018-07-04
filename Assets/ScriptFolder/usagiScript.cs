a2de0a83c55f2f9e233557c57d3a505284b48375
Eda Hikari <m1m18306@st.oit.ac.jp>
2018-07-04 21:55:42 +0900

    first unity commit

---
+++ Assets/ScriptFolder/usagiScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class usagiScript : MonoBehaviour {
+
+	Vector3 vec = new Vector3();
+	CharacterController charaCon ;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vec.x = Input.GetAxis("Horizontal");
+		vec.z = Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+	}
+}
